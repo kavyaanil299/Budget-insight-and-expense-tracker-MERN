cfcc519a26e11f1a66d080882b2cd5b580a04707
kavya <kavyaanil299@gmail.com>
2026-03-28 12:19:52 +0530

    added root route for render

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,7 @@ app.use("/api/transactions", transactionRoutes);
 app.use("/api/ai", aiRoutes);
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
