b85cb534f47037463d91fcec70410f8bababc00e
kavya <kavyaanil299@gmail.com>
2026-03-28 10:42:22 +0530

    fix vercel + api url

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://budget-insight-and-expense-tracker-mern.onrender.com"
+  baseURL:  "https://budget-insight-and-expense-tracker-mern.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
